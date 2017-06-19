51ca666be6a78102a53590998a85bd232959643b
我是一个TableView <18511654507@163.com>
2017-06-19 15:37:04 +0800

    Initial Commit

---
+++ NSProxyDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  NSProxyDemo
+//
+//  Created by ZhengWei on 2017/6/19.
+//  Copyright © 2017年 ZhengWei. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
